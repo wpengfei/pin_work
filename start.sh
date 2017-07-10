1021e82abe9845876900e740cf865e0c66a16446
wpengfei <wpengfeinudt@gmail.com>
2017-07-10 12:54:40 +0800

    Changed output color for replay.

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ rm obj-ia32/replay.so
 make PIN_ROOT=../pin-3.2-81205-gcc-linux/  obj-ia32/pintool_main.so
 echo ================================================
 
-time ../pin-3.2-81205-gcc-linux/pin -t obj-ia32/pintool_main.so -- ~/Desktop/pin_work/main_bank_nolock
+time ../pin-3.2-81205-gcc-linux/pin -t obj-ia32/pintool_main.so -- ~/Desktop/pin_work/main_bank
 echo ================================================
 
 make PIN_ROOT=../pin-3.2-81205-gcc-linux/  obj-ia32/replay.so
-time ../pin-3.2-81205-gcc-linux/pin -t obj-ia32/replay.so -- ~/Desktop/pin_work/main_bank_nolock
+time ../pin-3.2-81205-gcc-linux/pin -t obj-ia32/replay.so -- ~/Desktop/pin_work/main_bank
 
 #head pinatrace.out
